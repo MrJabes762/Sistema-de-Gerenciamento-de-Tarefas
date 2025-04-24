587ccb5f64dd79d2dad17d8bc844b1d155e7c9ec
Mr Jabes <jabessilva31@gmail.com>
2025-04-24 19:52:51 -0300

    adiciona repositório de tarefas como singleton com operações CRUD

---
+++ src/main/java/data/TarefasRepository.java
@@ -0,0 +1,71 @@
+package data;
+
+import java.util.HashMap;
+import java.util.Map;
+import model.Tarefa;
+
+/**
+ * Repositório de tarefas que atua como um singleton. Este repositório é
+ * responsável por armazenar e gerenciar as tarefas.
+ */
+public class TarefasRepository {
+
+    private static TarefasRepository instance;
+    private Map<Integer, Tarefa> tarefas;
+
+    private TarefasRepository() {
+        setTarefas(new HashMap<>());
+    }
+
+    public static TarefasRepository getInstance() {
+        if (instance == null) {
+            synchronized (TarefasRepository.class) {
+                if (instance == null) {
+                    instance = new TarefasRepository();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String adicionarTarefa(Tarefa tarefa) {
+        getTarefas().put(tarefa.getIdlocal(), tarefa);
+        return getTarefasCadastradas();
+    }
+
+    public String getTarefasCadastradas() {
+        StringBuilder sb = new StringBuilder("Tarefas cadastradas:\n");
+        for (Tarefa tarefa : getTarefas().values()) {
+            sb.append("ID: ").append(tarefa.getIdlocal())
+                    .append(", Data de Criação: ").append(tarefa.getDataCriacao())
+                    .append(", Título: ").append(tarefa.getTitulo())
+                    .append(", Descrição: ").append(tarefa.getDescricao())
+                    .append(", Prioridade: ").append(tarefa.getPrioridade().toString())
+                    .append(", Concluída: ").append(tarefa.isConcluida())
+                    .append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String removerTarefa(Tarefa tarefa) {
+        getTarefas().remove(tarefa.getIdlocal());
+        return getTarefasCadastradas();
+    }
+
+    public String atualizarTarefa(Tarefa tarefa) {
+        adicionarTarefa(tarefa);
+        return getTarefasCadastradas();
+    }
+
+    public Tarefa getTarefaPorId(int id) {
+        return getTarefas().get(id);
+    }
+
+    private Map<Integer, Tarefa> getTarefas() {
+        return tarefas;
+    }
+
+    private void setTarefas(Map<Integer, Tarefa> tarefas) {
+        this.tarefas = tarefas;
+    }
+}
