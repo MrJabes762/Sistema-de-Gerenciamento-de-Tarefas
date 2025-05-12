2c428bb79fc9f37faad859b26246c474fe5d3cc6
Mr Jabes <jabessilva31@gmail.com>
2025-05-12 18:20:23 -0300

    Refactor: Adiciona tratamento de exceções nas operações de tarefa no TarefasController

--- src/main/java/controller/TarefasController.java
+++ src/main/java/controller/TarefasController.java
@@ -1,12 +1,14 @@
 package controller;
 
+import model.Prioridade;
 import model.Tarefa;
 import service.TarefasService;
 
-
 /**
- * Controlador de tarefas que atua como um front controller e um façade com singleton.
- * Este controlador é responsável por gerenciar as operações relacionadas às tarefas,
+ * Controlador de tarefas que atua como um front controller e um façade com
+ * singleton.
+ * Este controlador é responsável por gerenciar as operações relacionadas às
+ * tarefas,
  * como adicionar, remover, atualizar e listar tarefas.
  */
 public class TarefasController implements Controller<Tarefa> {
@@ -16,6 +18,7 @@ public class TarefasController implements Controller<Tarefa> {
     private TarefasController() {
         setIntanceService(TarefasService.getInstance());
     }
+
     public static TarefasController getInstance() {
         if (instance == null) {
             synchronized (TarefasController.class) {
@@ -26,30 +29,53 @@ public class TarefasController implements Controller<Tarefa> {
         }
         return instance;
     }
+
     @Override
-    public String postTarefa(Tarefa tarefa) throws IllegalArgumentException {
-        return getIntanceService().adicionarTarefa(tarefa);
+    public String postTarefa(Tarefa tarefa) {
+        try {
+            return getIntanceService().adicionarTarefa(tarefa);
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
     }
+
     @Override
-    public String deleteTarefa(Tarefa tarefa) throws IllegalArgumentException {
-        return getIntanceService().removerTarefa(tarefa);
+    public String deleteTarefa(Tarefa tarefa) {
+        try {
+            return "A tarefa Foi removida com sucesso! " + getIntanceService().removerTarefa(tarefa);
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
     }
+
     @Override
-    public String putTarefa(Tarefa tarefa) throws IllegalArgumentException{
-        return getIntanceService().atualizarTarefa(tarefa);
+    public String putTarefa(Tarefa tarefa) {
+        try {
+            return getIntanceService().atualizarTarefa(tarefa);
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
     }
+
     @Override
     public String getlistTarefas() {
         return getIntanceService().listarTarefas();
     }
+
     @Override
-    public String concluirTarefa(Tarefa tarefa) throws IllegalArgumentException {
-        return getIntanceService().concluirTarefa(tarefa);
+    public String concluirTarefa(Tarefa tarefa) {
+        try {
+            return getIntanceService().concluirTarefa(tarefa);
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
     }
+
     @Override
     public Tarefa findById(int id) throws IllegalArgumentException {
         return getIntanceService().getTarefaPorId(id);
     }
+
     private static TarefasService getIntanceService() {
         return intanceService;
     }
@@ -57,4 +83,6 @@ public class TarefasController implements Controller<Tarefa> {
     private static void setIntanceService(TarefasService intanceService) {
         TarefasController.intanceService = intanceService;
     }
+
+    
 }
